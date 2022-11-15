5db40966d2da358b010df18d0de40829ada6039d
Jhoe73 <jhoenascimento73@gmail.com>
2022-11-15 12:47:24 -0300

    Create Vue Project Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!"/>
+    </div>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
